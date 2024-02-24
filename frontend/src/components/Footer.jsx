a80dd0e32af8684ff071c771fb286ffcc678f311
DARSHAN S V <130700972+DARSHANSV15@users.noreply.github.com>
2024-02-25 00:40:09 +0530

    Update css and js

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import "../../public/footer.css"
 
+const year = new Date().getFullYear();
+
 const Footer = () => {
     return (<>
         <div className="container myClass">
             <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-3 border-top px-5">
-                <p className="col-md-4 mb-0 text-muted">&copy; 2021 Company, Inc</p>
+                <p className="col-md-4 mb-0 text-muted">&copy; {year} Company, Inc</p>
 
                 <a href="/" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
                 <i className="fa-solid fa-leaf" style={{fontSize:"3rem"}}></i> 
