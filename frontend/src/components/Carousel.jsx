c159318bbce46869941c85b6f36a0603a808024c
DARSHAN S V <130700972+DARSHANSV15@users.noreply.github.com>
2024-02-24 19:42:42 +0530

    Update css

--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Carousel = () => {
   return (
     <div>
-      <div id="carouselExampleIndicators" className="carousel slide">
-  <div className="carousel-indicators px-5">
+      <div id="carouselExampleIndicators" className="carousel slide px-5">
+  <div className="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
