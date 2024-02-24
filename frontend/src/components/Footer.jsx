99134298b18aacba7730a10ed5a5cfa41fc29e4f
DARSHAN S V <130700972+DARSHANSV15@users.noreply.github.com>
2024-02-24 22:50:09 +0530

    update

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import "../../public/footer.css"
 
 const Footer = () => {
     return (<>
-        <div className="container">
+        <div className="container myClass">
             <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-3 border-top px-5">
                 <p className="col-md-4 mb-0 text-muted">&copy; 2021 Company, Inc</p>
 
