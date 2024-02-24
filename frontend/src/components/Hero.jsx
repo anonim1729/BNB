c747a53f3bc30d2387c4bb7499a601bba91b299b
DARSHAN S V <130700972+DARSHANSV15@users.noreply.github.com>
2024-02-24 19:49:17 +0530

    update hero

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -7,10 +7,6 @@ const Hero = () => {
     <h1 className="display-4 fw-bold text-body-emphasis">Centered screenshot</h1>
     <div className="col-lg-6 mx-auto">
       <p className="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
-      <div className="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
-        <button type="button" className="btn btn-primary btn-lg px-4 me-sm-3" fdprocessedid="0z0fmk">Primary button</button>
-        <button type="button" className="btn btn-outline-secondary btn-lg px-4" fdprocessedid="ljng9a">Secondary</button>
-      </div>
     </div>
     <div className="overflow-hidden" style={{maxHeight: "30vh;"}}>
       <div className="container px-5">
